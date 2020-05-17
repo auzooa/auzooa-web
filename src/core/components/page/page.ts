4e2ba5e13f6ba7ffa445be438c8f82128f452d3c
César Alberca <cesalberca@gmail.com>
2020-05-17 17:30:07 +0200

    Add wrapper

--- src/core/components/page/page.ts
+++ src/core/components/page/page.ts
@@ -11,12 +11,20 @@ export class Page extends LitElement {
           width: 100%;
           min-height: 100vh;
           background-color: var(--background-color);
+          color: var(--on-background-color);
+        }
+
+        .wrapper {
+          margin: 0 auto;
+          max-width: 40rem;
         }
       `
     ]
   }
 
   render() {
-    return html`<main class="page"><slot></slot></main>`
+    return html`<main class="page">
+      <div class="wrapper"><slot></slot></div>
+    </main>`
   }
 }
